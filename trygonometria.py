1acfd3098c01acbf97e5b0d0a78b0bd24cdaed60
Zofia Krasińska-Krawet <z.krasinska@gmail.com>
2016-11-15 15:39:50 +0100

    trygonometria - dodanie cosinusa

--- trygonometria.py
+++ trygonometria.py
@@ -11,3 +11,19 @@ def sin(x):
         return 0
     else:
         return False
+
+def cos(x):
+    if x == 90:
+        return 0
+    elif x == 60:
+        return 0.5
+    elif x == 45:
+        return (2**(0.5))/2
+    elif x == 30:
+        return (3**(0.5))/2
+    elif x == 0:
+        return 1
+    else:
+        return False
+
+
